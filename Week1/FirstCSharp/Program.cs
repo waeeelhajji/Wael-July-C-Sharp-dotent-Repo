898f03b80f815d43f374d792b4c69cbac4085454
waeeelhajji <waelhajji.contact@gmail.com>
2024-07-04 20:40:46 +0100

    Week1

--- Week1/FirstCSharp/Program.cs
+++ Week1/FirstCSharp/Program.cs
@@ -35,9 +35,63 @@ Console.WriteLine(groceryList2[2]);
 Console.WriteLine("it will change from this line ");
 groceryList2[2] = "Ham";
 Console.WriteLine(groceryList2[2]);
-// List is variable length
+
+//? List is variable length
+//! JS var numberList = [];
+List<int> numberList = new List<int>();
+//? Add to our List
+numberList.Add(3);
+numberList.Add(4);
+numberList.Add(1);
+numberList.Add(2);
+numberList.Add(0);
+numberList.Add(5);
+
+//? First Parameter is the index, seconde is the value
+numberList.Insert(1, 10000);
+foreach (int i in numberList)
+{
+    Console.WriteLine(i);
+
+}
+//? Remove from List
+numberList.RemoveAt(2);
+numberList.Remove(0);
+Console.WriteLine("--------------------------");
+foreach (int i in numberList)
+{
+    Console.WriteLine(i);
+
+}
+
+//! String vs Char 
+//? Strings contains multiple letters
+string words = "Hello";
+
+//? Char only contains 1 Letter
+//! And specifically uses single quotations
+char singleChar = 'C';
 
 
+//!----------------Functions
+//?--------------Parameters
+static int DoMath(int x, int y)
+{
+    return x + y;
+
+}
+
+//?--Arguments
+DoMath(5, 4);
+Console.WriteLine("-----------Return---------------");
+Console.WriteLine(DoMath(85, 74));
+
+static void SayHello()
+{
+    Console.WriteLine("Hello Dotnet");
+}
+Console.WriteLine("-----------Void---------------");
+SayHello();
 
 
 
