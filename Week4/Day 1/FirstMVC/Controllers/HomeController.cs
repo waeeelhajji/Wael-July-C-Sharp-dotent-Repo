3053810ad1b9230b0e45b5247a57f6bedf8c4c8d
waeeelhajji <waelhajji.contact@gmail.com>
2024-07-23 20:41:36 +0100

    Week4Day1

---
+++ Week4/Day 1/FirstMVC/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FirstMVC.Models;
+
+namespace FirstMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    public static List<Pet> Pets = new List<Pet>();
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost("create/pet")]
+    public IActionResult Create(Pet newPet)
+    {
+        if (ModelState.IsValid)
+        {
+            Pets.Add(newPet);
+            return RedirectToAction("AllPets");
+        }
+        else
+        {
+            // Render validation Errors
+            return View("index");
+        }
+    }
+    [HttpGet("AllPest")]
+    public IActionResult AllPets()
+    {
+        return View("AllPets", Pets);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Week4/Day 1/FirstMVC/Models/Pet.cs
@@ -0,0 +1,19 @@
+#pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
+namespace FirstMVC.Models;
+
+
+public class Pet
+{
+    [Required(ErrorMessage = " Name is Important")]
+    [MinLength(2)]
+    public string Name { get; set; }
+    [Required]
+    [MinLength(2)]
+    [MaxLength(20)]
+    public string Species { get; set; }
+    public bool IsFriendly { get; set; }
+    [Required]
+    [Range(0, int.MaxValue)]
+    public int Age { get; set; }
+}
