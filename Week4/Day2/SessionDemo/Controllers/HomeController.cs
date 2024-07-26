1180bdf0a69687eda40db9530f475aa6c371963a
waeeelhajji <waelhajji.contact@gmail.com>
2024-07-26 20:06:04 +0100

    Week4Day2

---
+++ Week4/Day2/SessionDemo/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SessionDemo.Models;
+
+namespace SessionDemo.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        //!Making Session
+        //? String
+        // HttpContext.Session.SetString("UserName", "Wael Hajji");
+        //? Number 
+        // HttpContext.Session.SetInt32("Num", 5);
+        return View();
+    }
+    [HttpPost("Login")]
+    public IActionResult Login(string Name, int Num)
+
+    {
+
+        HttpContext.Session.SetString("UserName", Name);
+        HttpContext.Session.SetInt32("Number", Num);
+        return RedirectToAction("Privacy");
+
+    }
+
+    public IActionResult Privacy()
+    {
+        //1
+        string UserName = HttpContext.Session.GetString("UserName");
+        //2
+        int? Number = HttpContext.Session.GetInt32("Number");
+
+        if (UserName == null)
+        {
+            return RedirectToAction("Index");
+
+        }
+        return View();
+    }
+
+    [HttpPost("logout")]
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index");
+
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
